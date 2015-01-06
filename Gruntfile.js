a8b5a383be27b9c4bffff1f879ce626fae7e1e75
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2015-01-06 04:11:11 +0100

    Make the 'dist' folder working.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function (grunt) {
     cssmin: {
       dist: {
         files: {
-          'dist/css/application.min.css': ['dist/css/application.min.css']
+          'dist/css/application.min.css': ['src/css/*.css']
         }
       }
     },
@@ -14,33 +14,13 @@ module.exports = function (grunt) {
           'dist/index.html': ['src/index.html']
         }
       }
-    },
-
-    uglify: {
-      dist: {
-        files: {
-          'dist/js/application.min.js': ['src/js/*.js']
-        }
-      }
-    },
-
-    uncss: {
-      dist: {
-        files: {
-          'dist/css/application.min.css': ['src/index.html']
-        }
-      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-processhtml');
-  grunt.loadNpmTasks('grunt-uncss');
 
   grunt.registerTask('default', [
-    'uglify',
-    'uncss',
     'cssmin',
     'processhtml',
   ]);
