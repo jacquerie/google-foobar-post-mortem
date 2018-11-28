8841d06c2b06a591db3acb7e0c985662693b201e
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-11-28 12:22:57 +0100

    Convert the project to use the new template.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,99 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
   grunt.initConfig({
     copy: {
       dist: {
         files: {
           'dist/favicon.ico': ['src/favicon.ico'],
-          'dist/apple-touch-icon.png': ['src/apple-touch-icon.png']
-        }
-      }
+          'dist/apple-touch-icon.png': ['src/apple-touch-icon.png'],
+        },
+      },
     },
 
     cssmin: {
       dist: {
         files: {
-          'dist/css/application.min.css': ['src/css/*.css']
-        }
-      }
+          'dist/css/application.min.css': ['dist/css/application.min.css'],
+        },
+      },
+    },
+
+    eslint: {
+      target: [
+        'Gruntfile.js',
+        'src/js/**/*.js',
+      ],
+    },
+
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        minifyJS: true,
+      },
+      dist: {
+        files: {
+          'dist/index.html': 'dist/index.html',
+        },
+      },
+    },
+
+    jsonlint: {
+      all: {
+        options: {
+          format: true,
+          indent: 2,
+        },
+        src: [
+          '.eslintrc.json',
+          'package.json',
+          'package-lock.json',
+        ],
+      },
     },
 
     processhtml: {
       dist: {
         files: {
-          'dist/index.html': ['src/index.html']
-        }
-      }
-    }
+          'dist/index.html': ['src/index.html'],
+        },
+      },
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'dist/js/application.min.js': ['src/js/**/*.js'],
+        },
+      },
+    },
+
+    uncss: {
+      dist: {
+        files: {
+          'dist/css/application.min.css': ['src/index.html'],
+        },
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-jsonlint');
   grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-uncss');
 
-  grunt.registerTask('default', [
-    'copy',
+  grunt.registerTask('build', [
+    'uglify',
+    'uncss',
     'cssmin',
     'processhtml',
+    'htmlmin',
+  ]);
+
+  grunt.registerTask('lint', [
+    'jsonlint',
+    'eslint',
   ]);
 };
